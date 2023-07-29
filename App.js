1c3ef2ffe92ee307f864e0920586e706f3dedd44
miriyala srikanth <123526232+srikanthmiriyala017@users.noreply.github.com>
2023-07-29 17:54:17 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Login from './components/Login';
+
+
+const App = () => {
+
+
+  return (
+    <div>
+      
+      <Login/>
+      
+    </div>
+  );
+};
+
+export default App;
